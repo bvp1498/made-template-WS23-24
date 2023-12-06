22cc890b3187286627ebae466c423ea0b9c37e29
Bhumi Rakeshkumar Patel <148459689+bvp1498@users.noreply.github.com>
2023-12-06 16:07:36 +0100

    Update pipeline.sh

--- project/pipeline.sh
+++ project/pipeline.sh
@@ -1,5 +1,14 @@
-
 #!/bin/bash
-python3 /project/process_datasets.py
 
+# Export kaggle.json to os env for Kaggle authentication
+KAGGLE_JSON_PATH="./project/kaggle.json"
+KAGGLE_CONFIG_DIR=$(dirname "$KAGGLE_JSON_PATH")
+export KAGGLE_CONFIG_DIR
+
+# Install required packages from requirements.txt
+pip install --upgrade pip
+pip install -r ./project/requirements.txt
+
+# Run your Python file
+python3 /project/process_datasets.py
 
